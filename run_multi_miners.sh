f2a3554fecaa25a7156f9185446cbd33e168d43b
lihaiquan <lihaiquan2009@126.com>
2025-05-23 10:08:25 +0800

    Nockchain挖矿单机多开脚本

---
+++ run_multi_miners.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+############################################
+## 作者:       0x13904b 
+## Twitter|X: https://x.com/0x13904b
+## Telegram:  https://t.me/twitter_0x13904b
+############################################
+
+
+# Check for help flag and parse arguments for starting node index
+NUM_NODES=1
+START_INDEX=1
+
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+  echo ""
+  echo "Script: run_multi_miners.sh"
+  echo "Usage: sh $0 [启动节点数量] [起始节点编号]"
+  echo "Example: sh $0 10 5   # 表示启动10个节点，起始编号为5"
+  echo "Info: [启动节点数量] [起始节点编号] 参数可选，默认为1, 参数需为大于0的整数。"
+  exit 0
+fi
+
+# Check if the number of nodes is provided as an argument
+if [ $# -eq 0 ]; then
+  echo "未提供启动节点数量和起始节点编号参数，默认启动一个节点，起始编号为1"
+  echo "**** 本次计划启动 $NUM_NODES 个节点，起始编号为 $START_INDEX ****"
+else
+  if [[ $1 =~ ^[1-9][0-9]*$ ]]; then
+    NUM_NODES=$1
+    if [ $# -eq 2 ] && [[ $2 =~ ^[1-9][0-9]*$ ]]; then
+      START_INDEX=$2
+    fi
+  else
+    echo "Error: 传入参数错误。 [启动节点数量] [起始节点编号] 参数需为大于0的整数。"
+    echo "执行命令: \`sh $0 -h\` 查看帮助信息"
+    exit 1
+  fi
+  echo "**** 本次计划启动 $NUM_NODES 个节点，起始编号为 $START_INDEX ****"
+fi
+
+
+# Create directories for each instance
+NODES=""
+echo ""
+echo "** 开始创建节点环境 **"
+for i in $(seq $START_INDEX $(($START_INDEX + $NUM_NODES - 1))); do
+  mkdir node$i
+  cp .env node$i/
+  NODES="$NODES ./node$i"
+  echo " node$i 节点环境创建完成！"
+done
+echo ""
+echo "** 节点环境准备完成，开始启动节点 **"
+
+# Run each instance in its own directory with .env loaded
+for i in $(seq $START_INDEX $(($START_INDEX + $NUM_NODES - 1))); do
+  cd node$i && sh ../scripts/run_nockchain_miner.sh &
+  echo " node$i 节点启动完成！"
+done
+
+echo ""
+echo "** 所有节点启动完成 **"
+echo "本次启动的节点环境目录为：$NODES"
+
