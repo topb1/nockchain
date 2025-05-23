ea1bdd7571e15fc076206db486b041ce3672cffb
lihaiquan <lihaiquan2009@126.com>
2025-05-23 23:18:34 +0800

    1. 添加依赖包检测安装 2. 适配最新版本（62d6494）教程

--- setMiner-Mac.sh
+++ setMiner-Mac.sh
@@ -4,59 +4,65 @@
 # Twitter|X: https://x.com/0x13904b
 # Telegram:  https://t.me/twitter_0x13904b
 
-if [ -z "$1" ]; then
-    PUB_KEY=""
-    echo "[INFO] 未设置公钥，默认会新生成钱包，将使用新公钥挖矿"
-    if [ -z "$PUB_KEY" ]; then
-        echo '\n-------------------------------------'
-    fi
-else
-    PUB_KEY=$1
-    if [[ $PUB_KEY =~ ^[23]{1}[0-9a-km-zA-HJ-NP-Z]{127}$ ]]; then
-        echo "[INFO] 使用传入公钥挖矿，当前公钥是: $PUB_KEY"
-    else
-        echo "[ERROR] 公钥不符合要求，请检查！！！！"
-        echo "[ERROR] 当前传入公钥是:$PUB_KEY"
-        exit 1
-    fi
+
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+  echo ""
+  echo "此脚本提供了 Mac 系统下的依赖包检测安装，以及 Nockchain 项目的编译和启动。适配官方库62d6494版本教程"
+  echo "使用方法:  sh $0 "
+  echo "注意事项: "
+  echo " 1. 启动前请手动删除已clone的 nockchain 文件夹，避免冲突。命令：rm -rf nockchain"
+  echo " 2. 脚本会自动新生成钱包并备份在./nockchain/keys.export，公钥会自动导入.env文件。"
+  echo "  2.1 nockchain-wallet show-master-pubkey   #可以查看当前钱包公钥。"
+  echo "  2.2 nockchain-wallet show-seedphrase      #可以查看当前钱包助记词。"
+  echo "  2.1 nockchain-wallet show-master-privkey  #可以查看当前钱包主私钥。"
+  echo " 3. nockchain安装进度请查看 tail -f ./nockchain/log.txt"
+  exit 0
 fi
 
 ##############################  新机器依赖包安装 代码 ###################################
-# # 检查并安装 Homebrew（macOS 的包管理器）
-# if ! command -v brew &> /dev/null; then
-#     echo "安装 Homebrew..."
-#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
-#     eval "$(/opt/homebrew/bin/brew shellenv)"
-# fi
+#  Mac 系统下检测并安装 Homebrew
+if ! command -v brew &> /dev/null; then
+    echo "安装 Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo "Homebrew 已安装：$(brew --version)"
+fi
 
-# # 检查并安装 Git（如果未安装 CLT）
-# if ! command -v git &> /dev/null; then
-#     echo "安装 Xcode 命令行工具以获取 Git..."
-#     xcode-select --install
-# fi
+#  Mac 系统下检测并安装 Xcode 命令行工具
+if ! command -v git &> /dev/null; then
+    echo "安装 Xcode 命令行工具..."
+    xcode-select --install
+else
+    echo "Xcode 命令行工具已安装"
+fi
 
-# # 检查并安装 Make（通过 CLT）
-# if ! command -v make &> /dev/null; then
-#     echo "安装 Xcode 命令行工具以获取 Make..."
-#     xcode-select --install
-# fi
+#  Mac 系统下检测并安装 Make
+if ! command -v make &> /dev/null; then
+    echo "安装 Make..."
+    xcode-select --install
+else
+    echo "Make 已安装"
+fi
 
-# # 检查 Curl（默认已存在）
-# if ! command -v curl &> /dev/null; then
-#     echo "安装 Curl..."
-#     brew install curl
-# else
-#     echo "Curl 已安装：$(curl --version | head -n 1)"
-# fi
+#  Mac 系统下检测 Curl
+if ! command -v curl &> /dev/null; then
+    echo "安装 Curl..."
+    brew install curl
+else
+    echo "Curl 已安装：$(curl --version | head -n 1)"
+fi
 
-# # 安装 Rust
-# if ! command -v rustc &> /dev/null; then
-#     echo "安装 Rust..."
-#     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-#     source $HOME/.cargo/env
-# fi
-
+#  Mac 系统下检测并安装 Rust
+if ! command -v rustc &> /dev/null; then
+    echo "安装 Rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+else
+    echo "Rust 已安装"
+fi
+exit 0
 # # 安装 Docker
 # # if ! command -v docker &> /dev/null; then
 # #     echo "安装 Docker..."
@@ -64,21 +70,23 @@ fi
 # #     echo "请手动启动 Docker Desktop 并完成设置。"
 # # fi
 
-# # 克隆 Nockchain 仓库
-# if [ ! -d "nockchain" ]; then
-#     git clone https://github.com/zorp-corp/nockchain
-# fi
-# cd nockchain
+# 克隆 Nockchain 仓库
+if [ ! -d "nockchain" ]; then
+    git clone https://github.com/zorp-corp/nockchain
+fi
+cd nockchain
+cp .env_example .env
 
 
 
 
 ##############################  编译以及启动挖矿的代码 ###################################
 
-#步骤 1： 安装 Choo（Jock/Hoon 编译器）
+#步骤 1： 安装 Hoonc（Jock/Hoon 编译器）
 ### 对应官方文档命令：make install-hoonc ###
 echo '1. make install-hoonc 开始' > ./log.txt
 make install-hoonc
+export PATH="$HOME/.cargo/bin:$PATH"
 echo '  ****** make install-hoonc 完成 ******' >> ./log.txt
 
 #步骤 2： 构建 Nockchain
@@ -91,17 +99,17 @@ echo '  ****** make build 完成 ******' >> ./log.txt
 ### 对应官方文档命令：make install-nockchain-wallet ###
 echo '3. make install-nockchain-wallet 开始' >> ./log.txt
 make install-nockchain-wallet
-echo '  ****** make build 完成 ******' >> ./log.txt
+echo '  ****** make install-nockchain-wallet 完成 ******' >> ./log.txt
 
 #步骤 4： 安装nockchain(install-nockchain) 
 ### 对应官方文档命令：make install-nockchain ###
 echo '4. make install-nockchain 开始' >> ./log.txt
 make install-nockchain
-echo '  ****** make build 完成 ******' >> ./log.txt
+echo '  ****** make install-nockchain 完成 ******' >> ./log.txt
 
 
 # 生成钱包&导出keys
-if [ -z "$PUB_KEY" ]; then
+# if [ -z "$PUB_KEY" ]; then
     echo '\n-------------------------------------' >> ./log.txt
     echo '  ****** 生成钱包 & 导出keys ******' >> ./log.txt
     nockchain-wallet keygen >> ./keygen.txt 2>&1
@@ -112,13 +120,13 @@ if [ -z "$PUB_KEY" ]; then
     sed -i -e "s/^MINING_PUBKEY=.*$/MINING_PUBKEY=${PUB_KEY}/g" .env
     echo "公钥替换完成,当前公钥为:$PUB_KEY"
     echo '-------------------------------------\n' >> ./log.txt
-fi
+# fi
 
 #步骤 5： 启动 nockchain 挖矿
-### 对应官方文档命令：nockchain --mining-pubkey <your_pubkey> --mine ###
+### 对应官方文档命令：sh ./scripts/run_nockchain_miner.sh ###
 echo '5. 启动 nockchain 挖矿开始' >> ./log.txt
-nockchain --mining-pubkey $PUB_KEY --mine > ./mining.log &2>&1 &
-echo '5. run nockchain with mining_pubkey完成' >> ./log.txt
+sh ./scripts/run_nockchain_miner.sh > ./mining.log 2>&1 &
+echo '5. 启动 nockchain 挖矿完成' >> ./log.txt
 echo '  ****** 启动 nockchain 完成 挖矿进行中... ******' >> ./log.txt
 
 echo "Nockchain 挖矿中...."
